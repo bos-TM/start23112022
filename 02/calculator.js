9370f1a105650b73daeb02e873202ea25cca5afd
Oleksandr Berezhnyi <oberezhnyi@gmail.com>
2022-12-16 23:40:41 +0200

    Refactored code of  calcultor.js and planets.js

--- 02/calculator.js
+++ 02/calculator.js
@@ -2,17 +2,17 @@ let firstNumber = parseFloat(prompt("Введіть перше число", 0));
 let operator = prompt("Введіть один з опереторів ( +, *, /, - ): ");
 let secondNumber = parseFloat(prompt("Введіть друге число", 0));
 
-if (secondNumber == 0) {
-  alert("!!!Ділити на нуль не можна!!!");
-}
-else if (operator == '+') {
+if (operator == '+') {
   alert(firstNumber + secondNumber);
 }
 else if (operator == '*') {
   alert(firstNumber * secondNumber);
 }
 else if (operator == '/') {
-  alert(firstNumber / secondNumber);
+  if (secondNumber == 0)
+    alert("!!!Ділити на нуль не можна!!!");
+  else
+    alert(firstNumber / secondNumber);
 }
 else if (operator == '-') {
   alert(firstNumber - secondNumber);
